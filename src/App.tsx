2b1e96c2497f4124ed496c4969cac312f1453295
Amirnaji Rivaz <naji.rivaz@gmail.com>
2023-12-07 11:19:17 +0330

    Display genres

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,23 @@ import GenreList from "./components/GenreList";
 
 function App() {
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`,
-      lg: `"nav nav" "aside main"` 
-    }}>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr"
+      }}
+    >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-      <GridItem area="aside">
-        <GenreList /> 
-      </GridItem>
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
+        </GridItem>
       </Show>
       <GridItem area="main">
         <GameGrid />
