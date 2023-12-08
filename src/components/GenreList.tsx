98f2bdc326248e9fa8e4c54b59d6afa27e42c481
Amirnaji Rivaz <naji.rivaz@gmail.com>
2023-12-08 08:42:58 +0330

    Show loading skeletons instead of a spinner while fetching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,13 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Skeleton, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data, isLoading, error } = useGenres();
-
+  
   if (error) return null;
 
-  if (isLoading) return <Spinner />;
+  if (isLoading) return <Skeleton height='600px' borderRadius={10} />;
 
   return (
     <List>
