d70cbf5bd445f81e2b930c1db77bb7a68b7c87a9
Amirnaji Rivaz <naji.rivaz@gmail.com>
2023-12-30 14:43:23 +0330

    Refactor: create lookup hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
 import { useState } from "react";
-import { Genre } from "./hooks/useGenres";
-import PlatformSelector from "./components/PlatformSelector";
-import { Platform } from "./hooks/usePlatforms";
-import SortSelector from "./components/SortSelector";
+import GameGrid from "./components/GameGrid";
 import GameHeading from "./components/GameHeading";
+import GenreList from "./components/GenreList";
+import NavBar from "./components/NavBar";
+import PlatformSelector from "./components/PlatformSelector";
+import SortSelector from "./components/SortSelector";
 
 // undefined: the absence of a value
 // null: the intentional absence of a value
