888fc6e9c2cfbe6a3f278b26cc25d173c64a4ed3
Amirnaji Rivaz <naji.rivaz@gmail.com>
2024-01-15 17:49:11 +0330

    Fetch game details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -22,9 +22,15 @@ class APIClient<T> {
 
   getAll = (config: AxiosRequestConfig) => {
     return axiosInstance
-    .get<FetchResponse<T>>(this.endpoint, config)
-    .then((res) => res.data);
-  }
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
 }
 
 export default APIClient;
