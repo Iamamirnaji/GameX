365d6959a570a45f9b4eb79c76ae3a8c28d0ff3f
Amirnaji Rivaz <naji.rivaz@gmail.com>
2024-01-16 08:12:49 +0330

    Build game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,9 +2,10 @@ import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailPage = () => {
-  const {slug} = useParams();
+  const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
 
   if (isLoading) return <Spinner />;
@@ -15,8 +16,9 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
-  )
-}
+  );
+};
 
-export default GameDetailPage
+export default GameDetailPage;
